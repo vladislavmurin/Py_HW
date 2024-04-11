69f8e4f1f88a918cb0a8775e4d4a482b4ed6ab4d
vladislavmurin <murin.vladislav@gmail.com>
2024-04-11 20:24:37 +0400

    namedtuple task updated

--- 04_DadaClasses/test_namedtuple.py
+++ 04_DadaClasses/test_namedtuple.py
@@ -1,5 +1,15 @@
 from collections import namedtuple
 
+# Also instead of IMPORT - it can be done via CLASS
+# class tuple_named:
+#     def __init__(self, user_id, user_name, ..... ):
+#         self._user_id = user_id
+#         self._user_name = user_name
+
+
+User = namedtuple('User', 'user_name, user_id')
+print(User)
+
 
 def named_tuple():
     pass

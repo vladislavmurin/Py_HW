493346a2a55cf21fdffdfe46f42f3febc1bdb7e6
vladislavmurin <murin.vladislav@gmail.com>
2024-04-11 19:08:03 +0400

    WSGL update

--- 03.WSGI/main.py
+++ 03.WSGI/main.py
@@ -1,3 +1,13 @@
-def WSGI_gunicorn(environ, start_response):
-    start_response('200 OK', [('Content-Type', 'text/plain')])
-    yield b'Hello World!\n'
+from pprint import pprint
+
+
+def app(environ, start_response):
+    pprint(environ)
+    start_response('200 OK', [('Content-Type', 'text/html')])
+    yield b'<h1>Hello World!</h1>\n'
+
+
+# Use Terminal:
+# gunicorn --bind 127.0.0.1:5000 main:WSGI_gunicorn
+# http GET 127.0.0.1.:8000/DOG
+# http POST 127.0.0.1.:8000/DOG
